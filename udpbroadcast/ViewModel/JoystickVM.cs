c4f5955c0584030a67ad200aa18cce2331606880
math039h <43374961+math039h@users.noreply.github.com>
2019-12-05 13:15:19 +0100

    add the project

---
+++ udpbroadcast/ViewModel/JoystickVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using udpbroadcast.Model;
+
+namespace udpbroadcast.ViewModel
+{
+    class JoystickVM : INotifyPropertyChanged
+    {
+        public int BrinKEr { get; set; }
+        public JoystickVM()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
